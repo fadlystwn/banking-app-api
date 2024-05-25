fd03ce959ea23152ab7c8cdb503eddd09dcad975
fadlystwn <fadlystwn@gmail.com>
2024-05-25 07:57:53 +0700

    adding prisma and configure connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService],
 })
-export class AppModule {}
+export class AppModule { }
