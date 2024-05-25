b0290d6b0339d13d59f169022a2a95284be6bd38
fadlystwn <fadlystwn@gmail.com>
2024-05-25 16:40:02 +0700

    adding jwt sign in generate token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
 
-  constructor(private readonly AuthService: AuthService) { }
+  constructor(private readonly authService: AuthService) { }
 
   @Post('/register')
   async register(@Body() createUserDto: CreateUserDto) {
-    return this.AuthService.register(createUserDto)
+    return this.authService.register(createUserDto)
   }
-
+  @HttpCode(HttpStatus.OK)
+  @Post('/login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password)
+  }
 }
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'd85ec80e59984974b49cb2ab9cd467750721fc5d22221340ef9b1a53170e8cfd'
+}
